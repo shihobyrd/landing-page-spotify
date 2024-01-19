74b2b9b73d71b232f37be1cb24a88f81d4a42477
Shiho Byrd <shihobyrd.m25@gmail.com>
2024-01-19 13:47:44 -0800

    mobile nav bg animation

--- app/js/script.js
+++ app/js/script.js
@@ -1,1 +1,19 @@
-console.log('hello world!');
+const btnHamburger = document.querySelector('#btnHamburger');
+const header = document.querySelector('.header');
+const overlay = document.querySelector('.overlay');
+
+document.querySelector('#btnHamburger').addEventListener('click', function () {
+    console.log('click hamburger');
+
+    if (header.classList.contains('open')) { // Close Hamburger Menu
+        header.classList.remove('open');
+        overlay.classList.add('fade-out');
+        overlay.classList.remove('fade-in');
+    }
+    else { // Open Hamburger Menu
+        header.classList.add('open');
+        overlay.classList.add('fade-in');
+        overlay.classList.remove('fade-out');
+    }
+
+});
