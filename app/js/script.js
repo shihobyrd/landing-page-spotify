f78a84e889ffe7a9f0d6e32bedfd4d72d90eebb0
Shiho Byrd <shihobyrd.m25@gmail.com>
2024-01-19 17:12:12 -0800

    nav menu responsive

--- app/js/script.js
+++ app/js/script.js
@@ -1,6 +1,7 @@
 const btnHamburger = document.querySelector('#btnHamburger');
 const header = document.querySelector('.header');
 const overlay = document.querySelector('.overlay');
+const menu = document.querySelector('.header__menu');
 
 document.querySelector('#btnHamburger').addEventListener('click', function () {
     console.log('click hamburger');
@@ -9,11 +10,15 @@ document.querySelector('#btnHamburger').addEventListener('click', function () {
         header.classList.remove('open');
         overlay.classList.add('fade-out');
         overlay.classList.remove('fade-in');
+        menu.classList.add('fade-out');
+        menu.classList.remove('fade-in');
     }
     else { // Open Hamburger Menu
         header.classList.add('open');
         overlay.classList.add('fade-in');
         overlay.classList.remove('fade-out');
+        menu.classList.add('fade-in');
+        menu.classList.remove('fade-out');
     }
 
 });
